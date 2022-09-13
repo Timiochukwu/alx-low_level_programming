300d1be8a35cb7067f7f4b4b9e3e1693a2b09f4a
timiochukwu <timiochukwu0@gmail.com>
2022-09-13 06:00:40 -0700

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * Main - Entry Point
+ *
+ *   * 
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     *
+ *
+ *      */
+
+
+
+int main(void)
+
+{
+
+		puts("_putchar");
+
+			return (0);
+
+}
